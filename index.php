039ed7bd2b286c6ff1b1a61d44406f05947f81f1
enikeishik <enikeishik@gmail.com>
2019-02-20 13:40:48 +0300

    feat: added ignore directives

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require_once 'Filesystem.php';
 require_once 'GlobalData.php';
 
 $gd = new \TestCodacy\GlobalData($_GET, $_POST, $_COOKIE, $_SERVER);
+//@codingStandardsIgnoreStart
 \var_dump($gd->getServer());
+//@codingStandardsIgnoreEnd
 
 \var_dump(\file_exists('a.txt'));
