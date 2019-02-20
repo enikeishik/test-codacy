f22d152dee85cf80004482f33ed5a2bf67f88dcb
enikeishik <enikeishik@gmail.com>
2019-02-20 13:27:51 +0300

    feat: made root ns for php functions

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once 'Filesystem.php';
 require_once 'GlobalData.php';
 
 $gd = new \TestCodacy\GlobalData($_GET, $_POST, $_COOKIE, $_SERVER);
-var_dump($gd->getServer());
+\var_dump($gd->getServer());
 
-var_dump(file_exists('a.txt'));
+\var_dump(\file_exists('a.txt'));
